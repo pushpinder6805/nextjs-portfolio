d754e210d14c598d16189330de9c0abadf8b1b6d
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2025-06-06 17:25:23 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Pushpender Chaudhary",
                 1000,
                 "Web Developer",
                 1000,
